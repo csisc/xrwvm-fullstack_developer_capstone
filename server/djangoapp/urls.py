fd2a86a0b39ec750707b8623cde0c86e7d97dfb3
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2025-03-08 13:58:12 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     # path for dealer reviews view
     
     # path for add a review view
-
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
