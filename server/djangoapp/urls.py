56fc744a154134a5303beedfbf3d8b8f9d00b28b
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2025-03-09 12:09:34 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,14 +7,14 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    # # path for registration
+    # path for registration
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='register'),
     path(route='get_cars', view=views.get_cars, name='getcars'),
-    
+
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(
@@ -32,7 +32,7 @@ urlpatterns = [
         view=views.get_dealer_reviews,
         name='dealer_details',
     ),
-    
+
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
